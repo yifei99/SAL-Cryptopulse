616a997dc0fc6d85de8e78ef54a1e362dedd3e26
Amit Kumar <103307184+aamitssharma07@users.noreply.github.com>
2024-11-12 17:23:35 -0600

    Create batch_processor.py
    Added a newCreate batch_processor.py file which do the same work as crypto_pulse_bigdata2024.sh

---
+++ cryptopulse/batch_processor.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+# Read dependencies from requirements.txt
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="cryptopulse",
+    version="0.1",
+    description="CryptoPulse: A model for cryptocurrency next day price forecasting using market sentiments and macroeconomic data",
+    author="Amit Kumar, Dr. Taoran Ji",
+    author_email="akumar3@islander.tamucc.edu, taoran.ji@tamucc.edu",
+    url="https://github.com/aamitssharma07/SAL-Cryptopulse.git",
+    packages=find_packages(),
+    install_requires=requirements,  # Load dependencies from requirements.txt
+    entry_points={
+    'console_scripts': [
+        'cryptopulse=cryptopulse.main:main',  # Single run
+        'cryptopulse_batch=cryptopulse.batch_processor:batch_process',  # Batch processing
+    ],
+},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
